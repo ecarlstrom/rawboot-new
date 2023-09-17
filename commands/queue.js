e164b7bbb535a39e89b197a93a46d25b00037a68
ecarlstrom <evan.carlstrom@gmail.com>
2023-09-16 20:47:29 -0400

    adds simple version of stop command to clear entire queue

--- commands/queue.js
+++ commands/queue.js
@@ -1,7 +1,7 @@
 const { EmbedBuilder, SlashCommandBuilder } = require('discord.js');
 const { useQueue } = require('discord-player');
 const cowboyIcon = process.env.ICON;
-// tomorrow: look into equalizer command for play as well
+// tomorrow: look into equalizer command for play as well, swap command?
 module.exports = {
     data: new SlashCommandBuilder()
         .setName('queue')
--- commands/stop.js
+++ commands/stop.js
@@ -3,26 +3,29 @@ const { GuildQueuePlayerNode, useQueue } = require('discord-player');
 
 module.exports = {
     data: new SlashCommandBuilder()
-    .setName('skip')
-    .setDescription('Skips the currently playing song'),
+    .setName('stop')
+    .setDescription('Stops and clears the song queue'),
     async execute(interaction) {
         const { guild } = interaction;
         const channel = interaction.member.voice.channel;
         const queue = useQueue(guild.id);
         const guildQueue = new GuildQueuePlayerNode(queue);
 
-        if (!channel) {
-            return interaction.reply('HEY TOP! STILL! JOIN A VOICE CHANNEL!');
-        } else if(!queue) {
+        // update this for after first stop
+        if(!queue || !GuildQueue) {
             return interaction.reply('No song currently playing.')
         }
 
-        var currentData = queue.currentTrack;
+
+        if (!channel) {
+            return interaction.reply('HEY TOP! STILL! JOIN A VOICE CHANNEL!');
+        } 
+
         await interaction.deferReply();
 
         try {
-            guildQueue.skip();
-            return interaction.followUp(`${currentData.title} by ${currentData.author} has been skipped. Please blame 👉 ${interaction.member.nickname} 👈 for any inconvenience.`);
+            guildQueue.stop(true);
+            return interaction.followUp(`Queue has been stopped and cleared. Please blame 👉 ${interaction.member.nickname} 👈 for any inconvenience.`);
         } catch (e) {
             return interaction.followUp(`UH OH! ${e.stack}`);
         }
